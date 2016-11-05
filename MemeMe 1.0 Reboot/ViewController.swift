312adc76d8e8eda896fe7a0630c4b69e0965dce9
Jaemoon <jaemoon@jaemoonpark.com>
2016-11-05 16:56:15 -0400

    add referencing outlets, toolbar, navigation bar, and textfields

--- MemeMe 1.0 Reboot/ViewController.swift
+++ MemeMe 1.0 Reboot/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var btmCamera: UIBarButtonItem!
+    @IBOutlet weak var btmAlbum: UIBarButtonItem!
+    @IBOutlet weak var btmShare: UIBarButtonItem!
+    @IBOutlet weak var btmCancel: UIBarButtonItem!
+    @IBOutlet weak var txtFieldTop: UITextField!
+    @IBOutlet weak var txtFieldBtm: UITextField!
+    @IBOutlet weak var viewImage: UIImageView!
+    @IBOutlet weak var barNavigation: UINavigationBar!
+    @IBOutlet weak var barTool: UIToolbar!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
